71eb9e8ada009a9dc069009e41de4f9f8fd98092
Raphaël Huchet <raph@selego.co>
2021-07-07 10:53:39 +0200

    hop

--- README.md
+++ README.md
@@ -1,33 +1,60 @@
 mongodb-anonymizer
 ==================
 
-Mongodb anonymzer tool
+MongoDB anonymzer tool.
+Export your MongoDB database anonymized from source to target. Replace all sensitive data thanks to `faker`.
 
-[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
 [![Version](https://img.shields.io/npm/v/mongodb-anonymizer.svg)](https://npmjs.org/package/mongodb-anonymizer)
-[![CircleCI](https://circleci.com/gh/rap2hpoutre/mongodb-anonymizer/tree/master.svg?style=shield)](https://circleci.com/gh/rap2hpoutre/mongodb-anonymizer/tree/master)
-[![Downloads/week](https://img.shields.io/npm/dw/mongodb-anonymizer.svg)](https://npmjs.org/package/mongodb-anonymizer)
+[![CircleCI](https://circleci.com/gh/rap2hpoutre/mongodb-anonymizer/tree/main.svg?style=svg)](https://circleci.com/gh/rap2hpoutre/mongodb-anonymizer/tree/main)
+[![Downloads](https://img.shields.io/npm/d/mongodb-anonymizer.svg)](https://npmjs.org/package/mongodb-anonymizer)
 [![License](https://img.shields.io/npm/l/mongodb-anonymizer.svg)](https://github.com/rap2hpoutre/mongodb-anonymizer/blob/master/package.json)
 
-<!-- toc -->
-* [Usage](#usage)
-* [Commands](#commands)
-<!-- tocstop -->
-# Usage
-<!-- usage -->
-```sh-session
-$ npm install -g mongodb-anonymizer
-$ mongodb-anonymizer COMMAND
-running command...
-$ mongodb-anonymizer (-v|--version|version)
-mongodb-anonymizer/0.0.0 darwin-x64 node-v14.15.3
-$ mongodb-anonymizer --help [COMMAND]
-USAGE
-  $ mongodb-anonymizer COMMAND
-...
-```
-<!-- usagestop -->
-# Commands
-<!-- commands -->
+## Usage
 
-<!-- commandsstop -->
+Run this command by giving a source URI and target URI (no need to install first thanks to `npx`):
+
+```bash
+npx mongodb-anonymizer \
+  --uri=mongodb://localhost:27017/source \
+  --targetUri=mongodb://localhost:27017/anonymized
+```
+
+☝️ Be careful, since target collections will be removed.
+
+### Specify list of fields to anonymize
+
+Use `--list` option with a comma separated list of column name:
+
+```bash
+npx mongodb-anonymizer  \
+  --uri=mongodb://localhost:27017/source \
+  --targetUri=mongodb://localhost:27017/anonymized
+  --list=email,firstName,lastName,phone
+```
+
+Specifying another list via `--list` replace the default automatically anonymized values:
+
+```csv
+email,name,description,address,city,country,phone,comment,birthdate
+```
+
+#### Customize replacements 
+
+You can also choose which faker function you want to use to replace data (default is `faker.random.word`):
+
+```bash
+npx mongodb-anonymizer  \
+  --uri=mongodb://localhost:27017/source \
+  --targetUri=mongodb://localhost:27017/anonymized
+  --list=firstName:faker.name.firstName,lastName:faker.name.lastName
+```
+
+:point_right: You don't need to specify faker function since the command will try to find correct function via column name.
+
+You can use plain text too for static replacements:
+```bash
+npx mongodb-anonymizer  \
+  --uri=mongodb://localhost:27017/source \
+  --targetUri=mongodb://localhost:27017/anonymized
+  --list=textcol:hello,jsoncol:{},intcol:12
+```
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,118 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
+import { MongoClient } from "mongodb";
+import * as faker from "faker";
 
 class MongodbAnonymizer extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    uri: flags.string({ char: "u", description: "mongodb source" }),
+    targetUri: flags.string({ char: "t", description: "mongodb target" }),
+    list: flags.string({
+      char: "l",
+      description: "list of columns to anonymize",
+      default:
+        "email,name,description,address,city,country,phone,comment,birthdate",
+    }),
+  };
 
   async run() {
-    const {args, flags} = this.parse(MongodbAnonymizer)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const { flags } = this.parse(MongodbAnonymizer);
+    if (!flags.uri || !flags.targetUri) {
+      this.error(
+        "You must specify a source and a target uri (type -h for help)"
+      );
     }
+
+    this.log("Connecting to source…");
+    const client = new MongoClient(flags.uri, { useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db();
+
+    this.log("Connecting to target…");
+    const targetClient = new MongoClient(flags.targetUri, {
+      useUnifiedTopology: true,
+    });
+    await targetClient.connect();
+    const targetDb = targetClient.db();
+
+    this.log("Getting collections…");
+    const collections = await db.listCollections().toArray();
+    this.log("Collections: " + collections.map((item) => item.name));
+
+    this.log("Anonymizing collections…");
+    for (const collection of collections) {
+      const collectionName = collection.name;
+      this.log("Anonymizing collection: " + collectionName);
+      const collectionData = await db
+        .collection(collectionName)
+        .find()
+        .toArray();
+      const list = flags.list.split(",");
+      const collectionDataAnonymized = await this.anonymizeCollection(
+        collectionData,
+        collectionName,
+        list
+      );
+
+      this.log("Inserting collection in target: " + collectionName);
+      await targetDb.collection(collectionName).deleteMany({});
+      // Save collection
+      await targetDb
+        .collection(collectionName)
+        .insertMany(collectionDataAnonymized);
+    }
+    this.log("Done!");
+
+    await client.close();
+    await targetClient.close();
+  }
+  async anonymizeCollection(
+    collectionData: any,
+    collectionName: any,
+    list: string[]
+  ) {
+    const collectionDataAnonymized = [];
+    const keysToAnonymize = list
+      .filter(
+        (item) => !item.includes(".") || item.startsWith(`${collectionName}.`)
+      )
+      .map((item) => item.replace(`${collectionName}.`, ""));
+    this.log(`Keys to anonymize: ${keysToAnonymize}`);
+    for (const document of collectionData) {
+      const documentAnonymized = {};
+      for (const key in document) {
+        if (!document) continue;
+        if (keysToAnonymize.includes(key)) {
+          documentAnonymized[key] = this.anonymizeValue(
+            document[key],
+            key.toLowerCase()
+          );
+        } else {
+          documentAnonymized[key] = document[key];
+        }
+      }
+      collectionDataAnonymized.push(documentAnonymized);
+    }
+    return collectionDataAnonymized;
+  }
+
+  anonymizeValue(value: any, key: any) {
+    if (key.includes("email")) return faker.internet.email();
+    if (key.endsWith("name")) return faker.name.findName();
+    if (key.includes("firstName")) return faker.name.firstName();
+    if (key.includes("lastName")) return faker.name.lastName();
+    if (key === "description") return faker.lorem.sentence();
+    if (key.endsWith("address")) return faker.address.streetAddress();
+    if (key.endsWith("city")) return faker.address.city();
+    if (key.endsWith("country")) return faker.address.country();
+    if (key.endsWith("phone")) return faker.phone.phoneNumber();
+    if (key.endsWith("comment")) return faker.lorem.sentence();
+    if (key.endsWith("date")) return faker.date.past();
+    return faker.random.word();
   }
 }
 
-export = MongodbAnonymizer
+export = MongodbAnonymizer;
